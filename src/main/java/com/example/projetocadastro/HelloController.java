f1c2906b9bc5f7f13e33a2bee96e9b16b9e33018
Lucas Machado Nunes <lucas072328@gmail.com>
2021-09-29 20:52:59 -0300

    A

--- src/main/java/com/example/projetocadastro/HelloController.java
+++ src/main/java/com/example/projetocadastro/HelloController.java
@@ -1,14 +1,37 @@
 package com.example.projetocadastro;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 
-public class HelloController {
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+    String[] siglas = {"AC","BA","CE","DF","PR","SP","RS","SC","TO"};
     @FXML
-    private Label welcomeText;
+    ComboBox<String> comboUF = new ComboBox();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    DatePicker dataNasc;
+
+    @FXML
+    CheckBox checkMasc,checkFem;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+            comboUF.getItems().addAll(siglas);
+    }
+    public void save(){
+        String select = comboUF.getSelectionModel().getSelectedItem();
+        System.out.println("UF selecionada: " + select);
+        LocalDate date = dataNasc.getValue();
+        System.out.println("Data Selecionada: " + date);
+        System.out.println(checkMasc.isSelected());
+        System.out.println(checkFem.isSelected());
     }
 }
