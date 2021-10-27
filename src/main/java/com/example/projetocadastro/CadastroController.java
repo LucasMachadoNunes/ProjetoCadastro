6abb07d586acb14b1ce4144d4c2128ec168bddfd
Lucas Machado Nunes <lucas072328@gmail.com>
2021-10-27 20:50:39 -0300

    Atualização

---
+++ src/main/java/com/example/projetocadastro/CadastroController.java
@@ -0,0 +1,89 @@
+package com.example.projetocadastro;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+
+public class CadastroController implements Initializable {
+    String[] siglas = {"AC","BA","CE","DF","PR","SP","RS","SC","TO"};
+
+    private TableView Cadastros;
+    private ObservableList Cadastros;
+
+    @FXML
+    ComboBox<String> comboUF = new ComboBox();
+
+    @FXML
+    DatePicker dataNasc;
+
+    @FXML
+    CheckBox checkMasc,checkFem;
+
+    @FXML
+    TableView<String> tabelacadastro = new TableView<>();
+
+    @FXML
+    TableColumn<String, String> colunanome;
+
+    @FXML
+    TableColumn<String, String> colunaSobrenome;
+
+    @FXML
+    TableColumn<String, String> colunaDtnasc;
+
+    @FXML
+    TableColumn<String, String> colunaEndereco;
+
+    @FXML
+    TableColumn<String, String> colunaCidade;
+
+    @FXML
+    TableColumn<String, String> colunaCep;
+
+    @FXML
+    TableColumn<String, String> colunaUF;
+
+    @FXML
+    TableColumn<String, String> colunaSexo;
+
+    @FXML
+    TableColumn<String, String> colunaEmail;
+
+    @FXML
+    TableColumn<String, String> colunaTelefone;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {comboUF.getItems().addAll(siglas);}
+    public void save(){
+        String select = comboUF.getSelectionModel().getSelectedItem();
+        System.out.println("UF selecionada: " + select);
+        LocalDate date = dataNasc.getValue();
+        System.out.println("Data Selecionada: " + date);
+        System.out.println(checkMasc.isSelected());
+        System.out.println(checkFem.isSelected());
+        carregarTableViewCadastros();
+    }
+}
+   public void carregarTableViewCadastros(){
+       List cadastros = new ArrayList();
+        TableColumncolunanome.setCellFactory(new PropertyValueFactory<>("Nome"));
+        TableColumncolunasobrenome.setCellFactory(new PropertyValueFactory<>("Sobrenome"));
+        list.add(new Cadastro("Juliana","Almeida"));
+        list.add(new Cadastro("Luciano","Gomes"));
+
+        observableListCadastros = FXCollections.observableArrayList(listCadastros);
+
+        tableViewCadastros.setItems(observableListCadastros);
+    }
--- src/main/java/com/example/projetocadastro/MainApp.java
+++ src/main/java/com/example/projetocadastro/MainApp.java
@@ -1,16 +1,18 @@
 package com.example.projetocadastro;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ProjetoCadastro.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("ProjetoCadastro.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Formulário de Cadastro");
         stage.setScene(scene);
